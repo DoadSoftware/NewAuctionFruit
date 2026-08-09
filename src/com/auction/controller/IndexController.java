d06fe99d6d039d946e44eb528d47f112613f7abf
Doad Software Department <doad.software@gmail.com>
2026-08-09 09:12:04 +0100

    Migrate JDK 17 & Tomcat 10.1

---
+++ src/com/auction/controller/IndexController.java
@@ -0,0 +1,125 @@
+package com.auction.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
+import jakarta.xml.bind.JAXBException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.auction.model.Auction;
+import com.auction.model.Player;
+import com.auction.service.AuctionService;
+import com.auction.util.AuctionUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.auction.util.AuctionFunctions;
+
+@Controller
+public class IndexController 
+{
+	@Autowired
+	AuctionService auctionService;
+
+	public static String expiry_date = "2026-12-31";
+	public static String current_date = "";
+	public static String error_message = "";
+	public static Auction session_auction;
+	public static Auction session_current_bid;
+	public static String session_selected_broadcaster;
+	public static boolean is_this_updating = false;
+	String teamName = "";
+	int rtmUsed = 0;
+	
+	@RequestMapping(value = {"/","/initialise"}, method={RequestMethod.GET,RequestMethod.POST}) 
+	public String initialisePage(ModelMap model) throws JAXBException, IOException, ParseException 
+	{
+		if(current_date == null || current_date.isEmpty()) {
+			current_date = AuctionFunctions.getOnlineCurrentDate();
+		}
+		
+		return "initialise";
+	}
+	@RequestMapping(value = {"/auction"}, method={RequestMethod.GET,RequestMethod.POST}) 
+	public String auctionPage(ModelMap model,
+			@RequestParam(value = "selectedBroadcaster", required = false, defaultValue = "") String selectedBroadcaster) 
+					throws JAXBException, IOException, ParseException 
+	{
+		if(current_date == null || current_date.isEmpty()) {
+			current_date = AuctionFunctions.getOnlineCurrentDate();
+		}
+		if(current_date == null || current_date.isEmpty()) {
+			model.addAttribute("error_message","You must be connected to the internet online");
+			return "error";
+		} else if(new SimpleDateFormat("yyyy-MM-dd").parse(expiry_date).before(new SimpleDateFormat("yyyy-MM-dd").parse(current_date))) {
+			model.addAttribute("error_message","This software has expired");
+			return "error";
+		}else {
+			session_selected_broadcaster = selectedBroadcaster;
+			model.addAttribute("session_selected_broadcaster", session_selected_broadcaster);
+			
+			session_current_bid = new Auction();
+			session_auction = new Auction();
+			
+			return "auction";
+		}
+	}
+	
+	@RequestMapping(value = {"/processAuctionProcedures"}, method={RequestMethod.GET,RequestMethod.POST})    
+	public @ResponseBody String processAuctionProcedures(
+			@RequestParam(value = "whatToProcess", required = false, defaultValue = "") String whatToProcess,
+			@RequestParam(value = "valueToProcess", required = false, defaultValue = "") String valueToProcess)
+					throws Exception
+	{	
+		switch (whatToProcess.toUpperCase()) {
+		case "READ-MATCH-AND-POPULATE":
+		    Auction auctionData = new ObjectMapper().readValue(new File(AuctionUtil.AUCTION_DIRECTORY + AuctionUtil.AUCTION_JSON), Auction.class);
+		    Auction currentBidData = new ObjectMapper().readValue(new File(AuctionUtil.AUCTION_DIRECTORY + AuctionUtil.CURRENT_BID_JSON), Auction.class);
+		    
+		    auctionData = AuctionFunctions.populateMatchVariables(auctionService, auctionData);
+		    auctionData.setTeamZoneList(AuctionFunctions.PlayerCountPerTeamZoneWise(auctionData.getTeam(),
+		    		auctionData.getPlayers(), auctionData.getPlayersList(),session_selected_broadcaster));
+		    teamName = "NO";
+		    for(int i=0; i<auctionData.getPlayersList().size()-1; i++) {
+		    	if(currentBidData.getCurrentPlayers().getPlayerId() == auctionData.getPlayersList().get(i).getPlayerId()) {
+		    		if(auctionData.getPlayersList().get(i).getLastYearTeam() != null) {
+		    			for(int j=0; j<=auctionData.getTeam().size()-1; j++) {
+		    				  if(auctionData.getTeam().get(j).getTeamId() == auctionData.getPlayersList().get(i).getLastYearTeam()) {
+		    					  for(Player auc : auctionData.getPlayers()) {
+		    							if(auctionData.getPlayersList().get(i).getLastYearTeam() == auc.getTeamId() && 
+		    									auc.getSoldOrUnsold().equalsIgnoreCase(AuctionUtil.RTM)) {
+		    								rtmUsed++;
+		    							}
+		    						}
+		    						if((Integer.valueOf(auctionData.getTeam().get(j).getTeamTotalRTM()) - rtmUsed) > 0) {
+		    							teamName = auctionData.getTeam().get(j).getTeamName1();
+		    							System.out.println(" tm = "+teamName);
+		    						}else {
+		    						}
+		    				  }
+		    			  }
+		    		}else {
+		    		}
+		    	}
+		    }
+		    rtmUsed = 0;
+		    
+		//    currentBidData.getCurrentPlayers().getLastYearTeam()
+		    Map<String, Object> response = new HashMap<>();
+		    response.put("auction", auctionData);
+		    response.put("currentBid", currentBidData);
+		    response.put("rtm", teamName);
+		   // response.put("auctionService", currentBidData);
+		    return new ObjectMapper().writeValueAsString(response);
+		default:
+		    return new ObjectMapper().writeValueAsString(session_current_bid);
+		}
+		
+	}
+}
---
+++ target/NewAuctionFruit-2.1/WEB-INF/views/initialise.jsp
@@ -0,0 +1,49 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="jakarta.tags.core"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
+  <title>Initialise Screen</title>
+	<script src="<c:url value='/webjars/jquery/3.7.1/dist/jquery.min.js'/>"></script>
+	<script src="<c:url value='/webjars/bootstrap/5.3.8/js/bootstrap.bundle.min.js'/>"></script>
+	<script src="<c:url value='/resources/javascript/index.js'/>"></script>
+	<link rel="stylesheet" href="<c:url value='/webjars/bootstrap/5.3.8/css/bootstrap.min.css'/>"/>
+</head>
+<body>
+<form:form name="initialise_form" autocomplete="off" action="auction" method="POST">
+<div class="content py-5" style="background-color: #EAE8FF; color: #2E008B">
+  <div class="container">
+	<div class="row">
+	 <div class="col-md-8 offset-md-2">
+       <span class="anchor"></span>
+         <div class="card card-outline-secondary">
+           <div class="card-header">
+             <h3 class="mb-0">Initialise</h3>
+           </div>
+          <div class="card-body">
+          <div id="initialise_div" style="display:none;">
+		   </div>
+			  <div class="form-group row row-bottom-margin ml-2" style="margin-bottom:5px;">
+			    <label for="selectedBroadcaster" class="col-sm-4 col-form-label text-left">Select Broadcaster </label>
+			    <div class="col-sm-6 col-md-6">
+			      <select id="selectedBroadcaster" name="selectedBroadcaster" class="browser-default custom-select custom-select-sm" 
+			      		onchange="processUserSelection(this)">
+			          <option value="HANDBALL">MT20</option>
+			      </select>
+			    </div>
+			  </div>
+			  &nbsp;
+		    <button style="background-color:#2E008B;color:#FEFEFE;" class="btn btn-sm" type="button"
+		  		name="load_scene_btn" id="load_scene_btn" onclick="processUserSelection(this)">
+		  		<i class="fas fa-film"></i> Load Scene</button>
+	       </div>
+	    </div>
+       </div>
+    </div>
+  </div>
+</div>
+</form:form>
+</body>
+</html>
